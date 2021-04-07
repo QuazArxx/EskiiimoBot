4cb3a0481706c68db37d5eccfeca78c6ec79ebc9
QuazArxx <33949974+QuazArxx@users.noreply.github.com>
2021-04-07 16:42:41 -0400

    finished command

--- commands/so.js
+++ commands/so.js
@@ -1,16 +1,12 @@
 module.exports = {
     name: 'so',
     description: 'Does a shoutout for the mentioned person',
-    execute(client, message, args, tags, user, channel, self) {
+    execute(client, message, args, user, channel, self) {
         let param = message.split(' ').filter(n => n);
-        const badges = tags.badges || {};
-        const isBroadcaster = badges.broadcaster;
-        const isMod = badges.moderator;
-        const isModUp = isMod || isBroadcaster;
-
-        if(!isModUp) return;
         if (!param[1]) return;
 
-        client.say(channel, `Make sure to give ${param[1]} a follow! Give ${param[1]} a follow at https://twitch.tv/${param[1]}`)
+        if(user.badges.moderator || user.badges.broadcaster) {
+            return client.say(channel, `Make sure to give ${param[1]} a follow! Give ${param[1]} a follow at https://twitch.tv/${param[1]}`)
+        } else return;   
     }
 }
