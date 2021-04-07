a75b50f36a6358595101e7a1dab4523c3dbce7bf
QuazArxx <33949974+QuazArxx@users.noreply.github.com>
2021-04-07 12:47:35 -0400

    added command to show EskiiimoJo's PSN

---
+++ commands/psn.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'psn',
+    description: 'Displays the psn',
+    execute(client, message, args, user, channel, self) {
+        client.say(channel, 'Hey you! Add me on PSN - Eskiimojo');
+    }
+}
