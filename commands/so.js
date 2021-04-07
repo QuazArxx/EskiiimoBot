a57383cc5ed1485b22e4c6ee9215b68b661014af
QuazArxx <33949974+QuazArxx@users.noreply.github.com>
2021-04-07 13:45:10 -0400

    Changes to mod check

--- commands/so.js
+++ commands/so.js
@@ -3,9 +3,12 @@ module.exports = {
     description: 'Does a shoutout for the mentioned person',
     execute(client, message, args, tags, user, channel, self) {
         let param = message.split(' ').filter(n => n);
-        let isBroadcaster = channel.replace('#', '') == user.username;
+        const badges = tags.badges || {};
+        const isBroadcaster = badges.broadcaster;
+        const isMod = badges.moderator;
+        const isModUp = isMod || isBroadcaster;
 
-        if(!(user.mod) || !(user['user-type']) === 'mod' || !isBroadcaster) return;
+        if(!isModUp) return;
         if (!param[1]) return;
 
         client.say(channel, `Make sure to give ${param[1]} a follow! Give ${param[1]} a follow at https://twitch.tv/${param[1]}`)
