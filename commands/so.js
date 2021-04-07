d1b8c33daa4ee7c3ea6ee29e1d9420f3138985e3
QuazArxx <33949974+QuazArxx@users.noreply.github.com>
2021-04-07 16:59:28 -0400

    Added removing the @ in link

--- commands/so.js
+++ commands/so.js
@@ -6,7 +6,9 @@ module.exports = {
         if (!param[1]) return;
 
         if(user.badges.moderator || user.badges.broadcaster) {
-            return client.say(channel, `Make sure to give ${param[1]} a follow! Give ${param[1]} a follow at https://twitch.tv/${param[1]}`)
+            if (param[1].startsWith('@')) {
+                return client.say(channel, `Make sure to give ${param[1].substring(1)} a follow! Give ${param[1].substring(1)} a follow at https://twitch.tv/${param[1].substring(1)}`)
+            } else return client.say(channel, `Make sure to give ${param[1]} a follow! Give ${param[1]} a follow at https://twitch.tv/${param[1]}`)
         } else return;   
     }
 }
