772d8e182ed19b4b524380dd37b4e0bab363eea2
QuazArxx <33949974+QuazArxx@users.noreply.github.com>
2021-04-08 13:00:28 -0400

    Added checking if the jail is empty

--- commands/breakout.js
+++ commands/breakout.js
@@ -4,10 +4,12 @@ module.exports = {
     name: 'hornyjail',
     descriptions: 'Sends someone to horny jail',
     execute(client, message, args, user, channel, self) {
+        if (functions.jail.length == 0) return;
+
         for (let x = 0; x < functions.jail.length; x++) {
             if (functions.jail[x] == user['display-name'].toLowerCase()) {
                 functions.jail.splice(x, 1);
-                client.say(channel, `${user['display-name']} broke out of horny jail!`);
+                return client.say(channel, `${user['display-name']} broke out of horny jail!`);
             }
         }
     }
