0e4d74b2452b476d3de439b4485a2a2dcaa5d5bb
QuazArxx <33949974+QuazArxx@users.noreply.github.com>
2021-04-08 12:40:43 -0400

    Added actually putting people in jail

--- commands/hornyjail.js
+++ commands/hornyjail.js
@@ -1,3 +1,5 @@
+const functions = require('../functions.js');
+
 module.exports = {
     name: 'hornyjail',
     descriptions: 'Sends someone to horny jail',
@@ -7,6 +9,7 @@ module.exports = {
         if(param[1] == 'all') {
             return client.say(channel, `${user['display-name']} decided that everyone needs to be in horny jail!`);
         } else {
+            functions.jail.push(param[1])
             client.say(channel, `${user['display-name']} sent ${param[1]} to horny jail.`);
         }
     }
