a68deb9ac6ece4c2b0daebdd02a9a0c0bcdfbfe8
QuazArxx <33949974+QuazArxx@users.noreply.github.com>
2021-04-08 12:40:27 -0400

    Changed to only work for people in jail

--- commands/breakout.js
+++ commands/breakout.js
@@ -1,7 +1,14 @@
+const functions = require('../functions.js');
+
 module.exports = {
     name: 'hornyjail',
     descriptions: 'Sends someone to horny jail',
     execute(client, message, args, user, channel, self) {
-        client.say(channel, `${user['display-name']} broke out of horny jail!`);
+        for (let x = 0; x < functions.jail.length; x++) {
+            if (functions.jail[x] == user['display-name'].toLowerCase()) {
+                functions.jail.splice(x, 1);
+                client.say(channel, `${user['display-name']} broke out of horny jail!`);
+            }
+        }
     }
 }
