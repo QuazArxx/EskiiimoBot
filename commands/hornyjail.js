d051709f81c51a899b9b1c7d69b48e812f63e7d7
QuazArxx <33949974+QuazArxx@users.noreply.github.com>
2021-04-10 09:29:53 -0400

    Changed so the bot can't get sent to jail

--- commands/hornyjail.js
+++ commands/hornyjail.js
@@ -6,7 +6,9 @@ module.exports = {
     execute(client, message, args, user, channel, self) {
         let param = message.split(' ').filter(n => n);
 
-        if(param[1] == 'all') {
+        if (param[1] == 'eskiiimobot') return client.say(channel, 'You can\'t jail the jailer!');
+
+        if (param[1] == 'all') {
             return client.say(channel, `${user['display-name']} decided that everyone needs to be in horny jail!`);
         } else if (param[1].startsWith('@')) {
             functions.jail.push(param[1].substring(1))
