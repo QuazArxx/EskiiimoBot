417aef7b2387b46f09d7258bdeebdddc59c11329
QuazArxx <33949974+QuazArxx@users.noreply.github.com>
2021-04-17 14:14:47 -0400

    Added pushing to lower case

--- commands/hornyjail.js
+++ commands/hornyjail.js
@@ -11,13 +11,13 @@ module.exports = {
         if (param[1] == 'all') {
             return client.say(channel, `${user['display-name']} decided that everyone needs to be in horny jail!`);
         } else if (param[1].startsWith('@')) {
-            functions.jail.push(param[1].substring(1))
+            functions.jail.push(param[1].substring(1).toLowerCase())
             client.say(channel, `${user['display-name']} sent ${param[1].substring(1)} to horny jail.`);
             setTimeout(function() {
                 return client.say(channel, `${param[1].substring(1)}, you are now in horny jail. Use !breakout to escape.`)
             }, 3000);
         } else {
-            functions.jail.push(param[1])
+            functions.jail.push(param[1].toLowerCase())
             client.say(channel, `${user['display-name']} sent ${param[1]} to horny jail.`);
             setTimeout(function() {
                 return client.say(channel, `${param[1]}, you are now in horny jail. Use !breakout to escape.`)
